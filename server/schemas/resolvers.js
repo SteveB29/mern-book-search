2614d5122f50f9b5e99c4a36de845e544ed96f8c
Steve Bendrick <steven.bendrick@gmail.com>
2021-12-19 11:01:27 -0500

    added me query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,20 @@
 const { User } = require('../models');
+// const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const foundUser = await User.findOne({
+          $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
+        });
 
+        return foundUser;
+      }
+
+      // throw new AuthenticationError('Not logged in');
+    }
   }
 };
 
