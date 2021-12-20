3c2409494f0a0a6f637e3c13cf90723a61fa4c06
Steve Bendrick <steven.bendrick@gmail.com>
2021-12-19 19:26:57 -0500

    added me query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,2 +1,20 @@
 import { gql } from "@apollo/client";
 
+export const QUERY_ME = gql`
+{
+  me {
+    _id
+    username
+    email
+    bookCount
+    savedBooks {
+      _id
+      bookId
+      image
+      title
+      authors
+      description
+    }
+  }
+}
+`
